ec670cb6576f15778fe6f87968585ffe8915c177
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2022-10-12 21:43:44 -0300

    main

--- index.js
+++ index.js
@@ -3,14 +3,27 @@
 const relacional = require('./service/relacional-service')
 const dw = require('./service/dw-service')
 
+//imports
+const gerarTempo = require('./src/GerarTempo')
+const loadAlunos = require('./src/LoadAlunos')
+const LoadCursos = require('./src/LoadCursos')
+const LoadDisciplinas = require('./src/LoadDisciplinas')
+const LoadFacts = require('./src/LoadFacts')
 
 async function main (){
-    const gerarTempo = require('./src/GerarTempo')
-    const loadAlunos = require('./src/LoadAlunos')
 
     //LOAD
     gerarTempo()  
     loadAlunos()
+    LoadCursos()
+    LoadDisciplinas()
+
+    LoadFacts()
+
+    
+    //selects
+    const resultQueryDW = await dw.ft_matriculas.findMany({})
+    console.log(resultQueryDW)
 
     const querySelect = await relacional.matriculas.findFirst({
         where: {
